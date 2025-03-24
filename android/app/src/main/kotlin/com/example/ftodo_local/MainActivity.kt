4bb2fcbe20a0066fefa252d1202bbf483fe3188a
sagar satapathy <125951803+sagarspt01@users.noreply.github.com>
2025-03-24 07:08:11 +0530

    structure_done

---
+++ android/app/src/main/kotlin/com/example/ftodo_local/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ftodo_local
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ftodo_local/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
